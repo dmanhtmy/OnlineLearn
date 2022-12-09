056eb44736756b27a4573c8de6e617dec284769e
vietanhhe151254 <anhnnvhe151254@fpt.edu.vn>
2022-12-09 23:08:37 +0700

    My Learn - Register Course (full)

---
+++ SWP391_OnlineLearn/src/java/Controllers/UserMylearningController.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controllers;
+
+import DAO.Impl.UserDAOImpl;
+import Models.Registration;
+import Models.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author windc
+ */
+public class UserMylearningController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        User user = (User) request.getSession().getAttribute("user");
+        UserDAOImpl userCourseDAO = new UserDAOImpl();
+        String courseName = request.getParameter("coursename");
+        String indexPage = request.getParameter("index");
+        if (indexPage == null) {
+            indexPage = "0";
+        }
+        int index = Integer.parseInt(indexPage);
+        //get course register by user
+        int count = userCourseDAO.getTotalMyCourse(user.getId(), courseName);
+        int endPage = count / 5;
+        if (count % 5 != 0) {
+            endPage++;
+        }
+        request.setAttribute("current_index", index);
+        request.setAttribute("endPage", endPage);
+        ArrayList<Registration> list = userCourseDAO.getMyCourseBySearch(index, user.getId(), courseName);
+        request.setAttribute("list", list);
+
+        request.setAttribute("user", user);
+        request.setAttribute("coursename", courseName);
+        String title_value = "ECOURSES - MY COURSES";
+        request.setAttribute("title_value", title_value);
+         request.getRequestDispatcher(request.getContextPath() + "/client/course/userCourseList.jsp").forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
