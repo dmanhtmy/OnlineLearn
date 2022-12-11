9eadf97675fa96ca2dfb12496db4a6d396e413d4
nguyenngoctuan <tuannnhe151047@fpt.edu.vn>
2022-12-11 18:26:42 +0700

    feat: CRUD Ads

---
+++ SWP391_OnlineLearn/src/java/Models/Ads.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author MrTuan
+ */
+public class Ads {
+    private int id;
+    private String image;
+    private String href;
+
+    public Ads() {
+    }
+
+    public Ads(int id, String image, String href) {
+        this.id = id;
+        this.image = image;
+        this.href = href;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+    
+}
