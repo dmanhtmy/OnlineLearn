efa820af7be88fb6162417972ba54f65f6c3b834
nguyenngoctuan <tuannnhe151047@fpt.edu.vn>
2022-12-11 19:07:33 +0700

    update: ads admin

--- SWP391_OnlineLearn/src/java/Models/Ads.java
+++ SWP391_OnlineLearn/src/java/Models/Ads.java
@@ -9,7 +9,9 @@ package Models;
  * @author MrTuan
  */
 public class Ads {
+
     private int id;
+    private String name_brand;
     private String image;
     private String href;
     private boolean status;
@@ -17,8 +19,16 @@ public class Ads {
     public Ads() {
     }
 
-    public Ads(int id, String image, String href, boolean status) {
+    public Ads(int id, String name_brand, String image, String href, boolean status) {
         this.id = id;
+        this.name_brand = name_brand;
+        this.image = image;
+        this.href = href;
+        this.status = status;
+    }
+
+    public Ads(String name_brand, String image, String href, boolean status) {
+        this.name_brand = name_brand;
         this.image = image;
         this.href = href;
         this.status = status;
@@ -30,6 +40,14 @@ public class Ads {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getName_brand() {
+        return name_brand;
+    }
+
+    public void setName_brand(String name_brand) {
+        this.name_brand = name_brand;
     }
 
     public String getImage() {
@@ -55,7 +73,4 @@ public class Ads {
     public void setStatus(boolean status) {
         this.status = status;
     }
-
-    
-    
 }
